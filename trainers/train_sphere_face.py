7586024866b7be6d12dbeb61c1c75e5af250f066
leonid <erlygin.la@phystech.edu>
2023-03-21 13:34:14 +0000

    docs

--- trainers/train_sphere_face.py
+++ trainers/train_sphere_face.py
@@ -2,7 +2,8 @@
 import sys
 sys.path.append("/app")
 
-
+# code based on original repo: https://github.com/MathsShen/SCF
+# main config for training: configs/hydra/train_sphere_face.yaml
 from lightning.pytorch.cli import LightningCLI
 
 # simple demo classes for your convenience
