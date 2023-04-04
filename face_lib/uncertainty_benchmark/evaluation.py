45887aedaca7cd6921307b9dce474c7eff3973d3
leonid <erlygin.la@phystech.edu>
2023-04-04 11:15:45 +0000

    final commit on extended branch

---
+++ face_lib/uncertainty_benchmark/evaluation.py
@@ -0,0 +1,18 @@
+
+import sys
+import hydra
+from pathlib import Path
+path = str(Path(__file__).parent.parent.parent.absolute())
+sys.path.insert(1, path)
+
+@hydra.main(
+    config_path=str(Path(".").resolve() / "configs/uncertainty_benchmark"),
+    config_name=Path(__file__).stem + "_default",
+    version_base="1.2",
+)
+def run(cfg):
+    pass
+
+
+if __name__ == "__main__":
+    run()
