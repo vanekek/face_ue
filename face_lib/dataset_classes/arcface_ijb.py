c6823cd6175db23e97879ea772132d3fccc33f3e
leonid <erlygin.la@phystech.edu>
2023-11-24 10:08:33 +0000

    interpolation

--- face_lib/dataset_classes/arcface_ijb.py
+++ face_lib/dataset_classes/arcface_ijb.py
@@ -1,9 +1,10 @@
 from torch.utils.data import DataLoader, Dataset
 import cv2
 import numpy as np
-
+from pathlib import Path
 from evaluation.embeddings import face_align_landmark
 from evaluation.data_tools import extract_meta_data
+from torchvision import transforms
 
 
 class IJB_aligned_images(Dataset):
@@ -12,34 +13,56 @@ class IJB_aligned_images(Dataset):
     ) -> None:
         super().__init__()
         self.norm_image = norm_image
-        (
-            templates,
-            medias,
-            p1,
-            p2,
-            label,
-            self.img_names,
-            self.landmarks,
-            face_scores,
-        ) = extract_meta_data(dataset_path, dataset_name)
+        img_list_path = (
+            Path(dataset_path) / "meta" / f"{dataset_name.lower()}_name_5pts_score.txt"
+        )
+        img_path = Path(dataset_path) / "loose_crop"
+        with open(img_list_path, "r") as ff:
+            # 1.jpg 46.060 62.026 87.785 60.323 68.851 77.656 52.162 99.875 86.450 98.648 0.999
+            img_records = np.array([ii.strip().split(" ") for ii in ff.readlines()])
+        self.img_names = np.array([img_path / ii for ii in img_records[:, 0]])
+        self.landmarks = img_records[:, 1:-1].astype("float32").reshape(-1, 5, 2)
+        self.transform = transforms.Compose(
+            [
+                transforms.ToPILImage(),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+            ]
+        )
+        # (
+        #     templates,
+        #     medias,
+        #     p1,
+        #     p2,
+        #     label,
+        #     self.img_names,
+        #     self.landmarks,
+        #     face_scores,
+        # ) = extract_meta_data(dataset_path, dataset_name)
 
     def __getitem__(self, index):
         img = self.img_names[index]
         landmark = self.landmarks[index]
         img = face_align_landmark(cv2.imread(str(img)), landmark)
-        img = (img - 127.5) * 0.0078125
         if self.norm_image:
-            img = (
-                img - np.array([0.5, 0.5, 0.5])[np.newaxis, np.newaxis, :]
-            ) / np.array([0.5, 0.5, 0.5])[np.newaxis, np.newaxis, :]
+            sample = self.transform(img)
+        # img = (img - 127.5) * 0.0078125
+        # if self.norm_image:
+        #     img = (
+        #         img - np.array([0.5, 0.5, 0.5])[np.newaxis, np.newaxis, :]
+        #     ) / np.array([0.5, 0.5, 0.5])[np.newaxis, np.newaxis, :]
 
-        return img.astype("float32")
+        return sample  # img.astype("float32")
 
     def __len__(self):
         return len(self.img_names)
 
 
 if __name__ == "__main__":
-    ds = IJB_aligned_images("/app/datasets/arcface_ijb/IJBC", "IJBC", norm_image=True)
+    pass
+    ds = IJB_aligned_images("/app/datasets/interpolation", "inter", norm_image=True)
     t = ds[0]
     x = 1
+    # ds = IJB_aligned_images("/app/datasets/arcface_ijb/IJBC", "IJBC", norm_image=True)
+    # t = ds[0]
+    # x = 1
