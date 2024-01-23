26b4330ba93469e341b4323d702ad0a13035e9f5
leonid <erlygin.la@phystech.edu>
2024-01-23 15:19:55 +0000

    new server

--- docker_scripts/launch_container.sh
+++ docker_scripts/launch_container.sh
@@ -1,2 +1,2 @@
-docker run -d --shm-size=8g --memory=80g --gpus=all --cpus=40 --user 1133:1134 --name face_eval_new --rm -it --init -v $HOME/face_ue:/app face-eval bash
+docker run -d --shm-size=8g --memory=80g --gpus=all --cpus=40 --user 1005:1005 --name face_eval_new --rm -it --init -v $HOME/face_ue:/app face-eval bash
 docker exec face_eval_new pip install -e .
